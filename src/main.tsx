b6e904567de3a4e41f790a3cdd4476f07881cfdb
Uday-Varikuppala <udayworks9@gmail.com>
2025-08-26 03:37:21 -0500

    feat: initial working portfolio setup with Vite, React, TS, Tailwind configured and site running

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
 
+
 // Mount the React application to the root div.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
